ce0f9e1cf7026cfa1c45edbbd9afab0346eeb482
KristofToth95 <sedes95@gmail.com>
2022-08-07 22:26:37 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Button(props) {
+  return <button className="button">{props.value}</button>;
+}
+
+function Screen(props) {
+  return (
+    <div className={props.className}>
+      <span>{props.value}</span>
+    </div>
+  );
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: null,
+    };
+  }
+  renderScreen() {
+    return <div>asdfasdf</div>;
+  }
+  render() {
+    return (
+      <div className="calculator">
+        <div className="toggle"></div>
+        <Screen className="screen" value="012012" />
+        <div className="keypad"></div>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
