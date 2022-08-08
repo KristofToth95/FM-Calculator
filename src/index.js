7204c4a72b7f5dc10d745c6e0f7b179a256720bd
KristofToth95 <sedes95@gmail.com>
2022-08-08 12:27:59 +0200

    displaying and styleing buttons on keypad

--- src/index.js
+++ src/index.js
@@ -14,6 +14,39 @@ function Screen(props) {
   );
 }
 
+class Keypad extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  renderButton(label, className) {
+    return <button className={className}>{label}</button>;
+  }
+  render() {
+    return (
+      <div className={this.props.className}>
+        {this.renderButton(7, "key")}
+        {this.renderButton(8, "key")}
+        {this.renderButton(9, "key")}
+        {this.renderButton("DEL", "key blue")}
+        {this.renderButton(4, "key")}
+        {this.renderButton(5, "key")}
+        {this.renderButton(6, "key")}
+        {this.renderButton("+", "key")}
+        {this.renderButton(1, "key")}
+        {this.renderButton(2, "key")}
+        {this.renderButton(3, "key")}
+        {this.renderButton("-", "key")}
+        {this.renderButton(".", "key")}
+        {this.renderButton(0, "key")}
+        {this.renderButton("/", "key")}
+        {this.renderButton("x", "key")}
+        {this.renderButton("RESET", "key blue reset")}
+        {this.renderButton("=", "key red equal")}
+      </div>
+    );
+  }
+}
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
@@ -28,8 +61,8 @@ class Calculator extends React.Component {
     return (
       <div className="calculator">
         <div className="toggle"></div>
-        <Screen className="screen" value="012012" />
-        <div className="keypad"></div>
+        <Screen className="screen" value="012,012" />
+        <Keypad className="keypad" />
       </div>
     );
   }
