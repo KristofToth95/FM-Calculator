e9c1d3f90103569d572ce4c15d1968f0e2581d52
KristofToth95 <sedes95@gmail.com>
2022-08-13 20:49:42 +0200

    displaying number on click and delete event

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function Button(props) {
-  return <button className="button">{props.value}</button>;
+  return (
+    <button
+      className={props.className}
+      onClick={() => props.onClick(props.value)}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 function Screen(props) {
+  let output = props.value.join("").replace(/.{3}/g, "$&,");
   return (
     <div className={props.className}>
-      <span>{props.value}</span>
+      <span>{output}</span>
     </div>
   );
 }
@@ -18,30 +26,35 @@ class Keypad extends React.Component {
   constructor(props) {
     super(props);
   }
-  renderButton(label, className) {
-    return <button className={className}>{label}</button>;
-  }
   render() {
     return (
       <div className={this.props.className}>
-        {this.renderButton(7, "key")}
-        {this.renderButton(8, "key")}
-        {this.renderButton(9, "key")}
-        {this.renderButton("DEL", "key blue")}
-        {this.renderButton(4, "key")}
-        {this.renderButton(5, "key")}
-        {this.renderButton(6, "key")}
-        {this.renderButton("+", "key")}
-        {this.renderButton(1, "key")}
-        {this.renderButton(2, "key")}
-        {this.renderButton(3, "key")}
-        {this.renderButton("-", "key")}
-        {this.renderButton(".", "key")}
-        {this.renderButton(0, "key")}
-        {this.renderButton("/", "key")}
-        {this.renderButton("x", "key")}
-        {this.renderButton("RESET", "key blue reset")}
-        {this.renderButton("=", "key red equal")}
+        <Button value={7} className="key" onClick={this.props.input} />
+        <Button value={8} className="key" onClick={this.props.input} />
+        <Button value={9} className="key" onClick={this.props.input} />
+        <Button value="DEL" className="key blue" onClick={this.props.input} />
+        <Button value={4} className="key" onClick={this.props.input} />
+        <Button value={5} className="key" onClick={this.props.input} />
+        <Button value={6} className="key" onClick={this.props.input} />
+        <Button value="+" className="key" onClick={this.props.input} />
+        <Button value={1} className="key" onClick={this.props.input} />
+        <Button value={2} className="key" onClick={this.props.input} />
+        <Button value={3} className="key" onClick={this.props.input} />
+        <Button value="-" className="key" onClick={this.props.input} />
+        <Button value="." className="key" onClick={this.props.input} />
+        <Button value={0} className="key" onClick={this.props.input} />
+        <Button value="/" className="key" onClick={this.props.input} />
+        <Button value="x" className="key" onClick={this.props.input} />
+        <Button
+          value="RESET"
+          className="key blue reset"
+          onClick={this.props.input}
+        />
+        <Button
+          value="="
+          className="key red equal"
+          onClick={this.props.input}
+        />
       </div>
     );
   }
@@ -51,18 +64,29 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      style: null,
+      numbers: Array(),
     };
   }
-  renderScreen() {
-    return <div>asdfasdf</div>;
+  handleInput(input) {
+    let value = this.state.numbers;
+    switch (true) {
+      case Number.isInteger(input):
+        value.push(input);
+        break;
+      case input == "DEL":
+        value.pop();
+        break;
+    }
+    this.setState({
+      numbers: value,
+    });
   }
   render() {
     return (
       <div className="calculator">
         <div className="toggle"></div>
-        <Screen className="screen" value="012,012" />
-        <Keypad className="keypad" />
+        <Screen className="screen" value={this.state.numbers} />
+        <Keypad className="keypad" input={(input) => this.handleInput(input)} />
       </div>
     );
   }
